aa2a0f9ac3f4cfab115fdb3811ce212da67919d5
hexzhen3x7 <hexzhen3x7@outlook.de>
2024-11-06 21:40:12 +0100

    Added Overlays: BananaPi M2 Berry Board Determiner \ Banana Pi M2 Berry System Alive LED BLINK Service + Script

---
+++ config/sources/debian-desktop.sh
@@ -0,0 +1,11 @@
+case $DESKTOP_ENVIRONMENT in
+    mate) desktopEnv="MATE" ;;
+    xfce) desktopEnv="XFCE" ;;
+    gnome) desktopEnv="GNOME" ;;
+    kde-plasma) desktopEnv="KDE Plasma" ;;
+    lxde) desktopEnv="LXDE" ;;
+    cinnamon) desktopEnv="Cinnamon" ;;
+    budgie) desktopEnv="Budgie" ;;
+    i3) desktopEnv="i3" ;;
+    *) desktopEnv="Unknown" ;;
+esac
